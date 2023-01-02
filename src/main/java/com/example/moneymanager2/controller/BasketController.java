bc1e1e8835b740888eb5318dbafe10c2c69ac4a2
DangNhatKhuong <nhatkhuong2001@gmail.com>
2023-01-02 20:43:00 +0700

    update deploy 8

---
+++ src/main/java/com/example/moneymanager2/controller/BasketController.java
@@ -0,0 +1,9 @@
+package com.example.moneymanager2.controller;
+
+import com.example.moneymanager2.service.BasketService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BasketController {
+    @Autowired
+    private BasketService basketService;
+}
